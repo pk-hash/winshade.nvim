e97ddfb9c16f52b5b2fe4ed63adc6847bf2f7a21
pk-hash <pk@hash>
2025-10-23 23:15:44 +0400

    perf: optimize window processing and add debouncing
    - Track last active window to only process 2 windows per event
    - Add 10ms debouncing for rapid WinEnter/BufWinEnter events
    - Separate fast path (2 windows) from full refresh path (all windows)
    - Clean up terminal match tracking for closed windows
    - Remove redundant window validity checks

--- lua/winshade/autocmd.lua
+++ lua/winshade/autocmd.lua
@@ -3,6 +3,17 @@ local M = {}
 local highlight = require("winshade.highlight")
 
 local augroup = nil
+local debounce_timer = nil
+
+local function debounced_apply()
+	if debounce_timer then
+		debounce_timer:stop()
+	end
+	debounce_timer = vim.defer_fn(function()
+		highlight.apply_to_inactive_windows()
+		debounce_timer = nil
+	end, 10)
+end
 
 M.setup = function()
 	augroup = vim.api.nvim_create_augroup("Winshade", { clear = true })
@@ -14,11 +25,18 @@ M.enable = function()
 		augroup = vim.api.nvim_create_augroup("Winshade", { clear = true })
 	end
 
-	vim.api.nvim_create_autocmd({ "WinEnter", "BufWinEnter", "FocusGained" }, {
+	-- Use debounced version for frequent events
+	vim.api.nvim_create_autocmd({ "WinEnter", "BufWinEnter" }, {
+		group = augroup,
+		callback = debounced_apply,
+	})
+
+	-- FocusGained should apply to all windows immediately
+	vim.api.nvim_create_autocmd("FocusGained", {
 		group = augroup,
 		callback = function()
 			vim.schedule(function()
-				highlight.apply_to_inactive_windows()
+				highlight.apply_to_all_inactive_windows()
 			end)
 		end,
 	})
@@ -28,13 +46,17 @@ M.enable = function()
 		callback = function()
 			highlight.setup()
 			vim.schedule(function()
-				highlight.apply_to_inactive_windows()
+				highlight.apply_to_all_inactive_windows()
 			end)
 		end,
 	})
 end
 
 M.disable = function()
+	if debounce_timer then
+		debounce_timer:stop()
+		debounce_timer = nil
+	end
 	if augroup then
 		vim.api.nvim_clear_autocmds({ group = augroup })
 	end
